66293a518144d9452c637993da1fae07f0f90ed7
变成光守护朝日小姐 <3026418423@qq.com>
2024-06-21 20:28:28 +0800

    delete unnecessary

--- src/test/java/cn/tju/sse/spring_backend/TestFileConfig.java
+++
@@ -1,6 +0,0 @@
-package cn.tju.sse.spring_backend;
-
-public class TestFileConfig {
-    public static final String fileRoot = "./src/test/Excels/";  // 必须以/结尾
-    public static final String ext = ".xlsx";
-}
